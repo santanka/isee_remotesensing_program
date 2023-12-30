4a06c7916dd359039e8dc3b511fe4fa98dc6a752
santanka <mkrock1062@gmail.com>
2023-12-31 02:23:52 +0900

    back_trajectory_manypoints_test.pyのバグ取り

--- back_trajectory/back_trajectory_manypoints/back_trajectory_manypoints_test.py
+++ back_trajectory/back_trajectory_manypoints/back_trajectory_manypoints_test.py
@@ -1,4 +1,5 @@
 import os
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import datetime
@@ -10,26 +11,27 @@ import pyproj
 import time
 import ftplib
 import xarray as xr
+import socket
 
 #####入力データの設定#####
 #始点の時刻(JST)
 start_year = 2020
-start_month = 8
-start_day = 22
+start_month = 7
+start_day = 8
 start_hour = 12
 
 #終点の時刻(JST)
 end_year = 2020
-end_month = 8
+end_month = 6
 end_day = 20
 end_hour = 12
 
 #始点の緯度経度範囲
-start_lon_min = 141.0
-start_lon_max = 142.0
-start_lat_min = 27.0
-start_lat_max = 28.0
-grid_width = 0.1
+start_lon_min = 141.9
+start_lon_max = 142.6
+start_lat_min = 27.5
+start_lat_max = 27.8
+grid_width = 0.05
 
 #計算・表示する緯度経度範囲
 pm_number = 2
@@ -42,11 +44,12 @@ nishinoshima_lat = 27.243889
 
 #データファイルの保存先のディレクトリ (形式: hoge/hogehoge)
 #プロットした図の保存先のディレクトリ (形式: hoge/hogehoge)
-#dir_figure = f''
+dir_data = f''
 #以下のようにすると、プログラムと同じディレクトリに保存される
-current_dir = os.path.dirname(os.path.abspath(__file__))
-dir_data = f'{current_dir}/data/back_trajectory_manypoints_test'
-dir_figure = f'{current_dir}/figure/back_trajectory_manypoints_test/{start_year}{start_month:02}{start_day:02}{start_hour:02}_{end_year}{end_month:02}{end_day:02}{end_hour:02}_{start_lat_min:.1f}_{start_lat_max:.1f}_{start_lon_min:.1f}_{start_lon_max:.1f}_{grid_width:.1f}'
+#current_dir = os.path.dirname(os.path.abspath(__file__))
+#dir_data = f'{current_dir}/data/back_trajectory_manypoints_test'
+#dir_figure = f'{current_dir}/figure/back_trajectory_manypoints_test/{start_year}{start_month:02}{start_day:02}{start_hour:02}_{end_year}{end_month:02}{end_day:02}{end_hour:02}_{start_lat_min:.1f}_{start_lat_max:.1f}_{start_lon_min:.1f}_{start_lon_max:.1f}_{grid_width:.1f}'
+dir_figure = f'/mnt/j/isee_remote_data/JST/back_trajectory_manypoints_test/{start_year}{start_month:02}{start_day:02}{start_hour:02}_{end_year}{end_month:02}{end_day:02}{end_hour:02}_{start_lat_min:.1f}_{start_lat_max:.1f}_{start_lon_min:.1f}_{start_lon_max:.1f}_{grid_width:.1f}'
 
 #並列処理の数をPCの最大コア数に設定
 parallel_number = os.cpu_count()
@@ -81,7 +84,11 @@ print(r'Figure will be saved in ' + dir_figure + r'.')
 print(r'Please wait for a while...')
 print(r'   ')
 
-#図の書式の指定
+#図の書式の指定(設定ではLaTeX調になっています)
+mpl.rcParams['text.usetex'] = True
+mpl.rcParams['font.family'] = 'serif'
+mpl.rcParams['font.serif'] = ['Computer Modern Roman']
+mpl.rcParams['mathtext.fontset'] = 'cm'
 plt.rcParams["font.size"] = 25
 
 #関数を定義
@@ -164,7 +171,24 @@ def get_grid_range_longitude(X, Y):
 #時間の変換
 def get_time_from_OPeNDAP():
     url_time = "https://tds.hycom.org/thredds/dodsC/GLBy0.08/expt_93.0/uv3z.ascii?time[0:1:12952]"
-    response_time = requests.get(url_time)
+    while True:
+        try:
+            response_time = requests.get(url_time, timeout=10)
+            response_time.raise_for_status()  # Raises an HTTPError if the status is 4xx, 5xx
+            break  # リクエストが成功したらループを抜ける
+        except requests.exceptions.HTTPError as errh:
+            print("Http Error:", errh)
+        except requests.exceptions.ConnectionError as errc:
+            print("Error Connecting:", errc)
+        except requests.exceptions.Timeout as errt:
+            print("Timeout Error:", errt)
+        except requests.exceptions.RequestException as err:
+            print("OOps: Something Else", err)
+
+        # エラーが発生した場合は10秒間待機して再試行
+        print("Wait for 10 seconds...")
+        time.sleep(10)
+    
     ascii_time = response_time.text.split("\n")
     time_line = None
     for i, line in enumerate(ascii_time):
@@ -208,20 +232,31 @@ def get_data(year_int, month_int, day_int, hour_int, lat_min, lat_max, lon_min, 
 
     #UTCに変換
     year_UTC, month_UTC, day_UTC, hour_UTC = JST_to_UTC(year=year_int, month=month_int, day=day_int, hour=hour_int)
+    if lat_min != lat_min or lat_max != lat_max or lon_min != lon_min or lon_max != lon_max:
+        print(r"Error: latitude or longitude is NaN.")
+        return None, None, None, None
     data_url = make_url(datetime.datetime(year_UTC, month_UTC, day_UTC, hour_UTC, 0, 0), lat_min, lat_max, lon_min, lon_max)
     if data_url == None:
         return None, None, None, None
     
-    #response = requests.get(data_url)をtry-exceptで囲み、エラーが出たら30秒後再度リクエストを送るようにする
+    #response = requests.get(data_url)をtry-exceptで囲み、エラーもしくは10秒で処理が進まなかったら30秒後再度リクエストを送るようにする
     while True:
         try:
-            response = requests.get(data_url)
-            break
-        except:
-            print("Error: requests.get(data_url)")
-            print("Wait for 30 seconds...")
-            time.sleep(30)
-            continue
+            response = requests.get(data_url, timeout=10)
+            response.raise_for_status()  # Raises an HTTPError if the status is 4xx, 5xx
+            break  # リクエストが成功したらループを抜ける
+        except requests.exceptions.HTTPError as errh:
+            print("Http Error:", errh)
+        except requests.exceptions.ConnectionError as errc:
+            print("Error Connecting:", errc)
+        except requests.exceptions.Timeout as errt:
+            print("Timeout Error:", errt)
+        except requests.exceptions.RequestException as err:
+            print("OOps: Something Else", err)
+
+        # エラーが発生した場合は10秒間待機して再試行
+        print("Wait for 10 seconds...")
+        time.sleep(10)
 
     ascii_data = response.text.split("\n")
 
@@ -291,6 +326,8 @@ def ocean_current_bilinear_interporation(year_int, month_int, day_int, hour_int,
     lon_max = longitude + diff_lon
 
     lat_data, lon_data, water_u_data, water_v_data = get_data(year_int, month_int, day_int, hour_int, lat_min, lat_max, lon_min, lon_max)
+    if lat_data is None or lon_data is None or water_u_data is None or water_v_data is None:
+        return np.nan, np.nan
 
     #latitude, longitudeの座標を囲む4点の座標を取得
     lon_1 = lon_data[np.where(lon_data < longitude)][-1]
@@ -331,6 +368,8 @@ def runge_kutta_method(year, month, day, hour, lon, lat):
 
     #k1
     water_u_k1, water_v_k1 = ocean_current_bilinear_interporation(date_k1.year, date_k1.month, date_k1.day, date_k1.hour, lat_initial, lon_initial)
+    if water_u_k1 != water_u_k1 or water_v_k1 != water_v_k1:
+        return np.nan, np.nan
     dx_k1 = water_u_k1 * - delta_t_double / 2
     dy_k1 = water_v_k1 * - delta_t_double / 2
     dr_k1 = np.sqrt(dx_k1**2 + dy_k1**2)
@@ -340,6 +379,8 @@ def runge_kutta_method(year, month, day, hour, lon, lat):
 
     #k2
     water_u_k2, water_v_k2 = ocean_current_bilinear_interporation(date_k2.year, date_k2.month, date_k2.day, date_k2.hour, lat_k1, lon_k1)
+    if water_u_k2 != water_u_k2 or water_v_k2 != water_v_k2:
+        return np.nan, np.nan
     dx_k2 = water_u_k2 * - delta_t_double / 2
     dy_k2 = water_v_k2 * - delta_t_double / 2
     dr_k2 = np.sqrt(dx_k2**2 + dy_k2**2)
@@ -349,6 +390,8 @@ def runge_kutta_method(year, month, day, hour, lon, lat):
 
     #k3
     water_u_k3, water_v_k3 = ocean_current_bilinear_interporation(date_k3.year, date_k3.month, date_k3.day, date_k3.hour, lat_k2, lon_k2)
+    if water_u_k3 != water_u_k3 or water_v_k3 != water_v_k3:
+        return np.nan, np.nan
     dx_k3 = water_u_k3 * - delta_t_double
     dy_k3 = water_v_k3 * - delta_t_double
     dr_k3 = np.sqrt(dx_k3**2 + dy_k3**2)
@@ -358,6 +401,8 @@ def runge_kutta_method(year, month, day, hour, lon, lat):
 
     #k4
     water_u_k4, water_v_k4 = ocean_current_bilinear_interporation(date_k4.year, date_k4.month, date_k4.day, date_k4.hour, lat_k3, lon_k3)
+    if water_u_k4 != water_u_k4 or water_v_k4 != water_v_k4:
+        return np.nan, np.nan
     
     water_u_slope = (water_u_k1 + 2 * water_u_k2 + 2 * water_u_k3 + water_u_k4) / 6E0
     water_v_slope = (water_v_k1 + 2 * water_v_k2 + 2 * water_v_k3 + water_v_k4) / 6E0
@@ -400,31 +445,82 @@ def download_netcdf(year, month, day, hour):
     ftp_base = os.path.basename(ftp_path)
     local_path = os.path.join(dir_data, ftp_base)
 
-    if (check_file_exists(local_path) == False):              #ファイルが存在しない場合、ダウンロードする
-        try:
-            with ftplib.FTP(ftp_site) as ftp:
-                ftp.login(user=ftp_user, passwd=ftp_password)   #ログイン
-                ftp.cwd('/')                                    #ルートディレクトリに移動
+    # ファイルが存在しない場合にダウンロードを試みる
+    if not check_file_exists(local_path):
+        while True:  # 無限に再試行
+            try:
+                # タイムアウトを設定（接続と読み取りのタイムアウト）
+                with ftplib.FTP(ftp_site, timeout=30) as ftp:
+                    ftp.login(user=ftp_user, passwd=ftp_password)  # ログイン
+                    ftp.cwd('/')  # ルートディレクトリに移動
 
-                with open(local_path, 'wb') as f:                 #ファイルをバイナリモードでダウンロード
-                    ftp.retrbinary(f'RETR {ftp_path}', f.write)
-                    print(r'Download file is ' + local_path)
-        except Exception as e:
-            print(f"Error downloading file: {e}")
-            return np.zeros((line_number, pixel_number))
+                    # ファイルをバイナリモードでダウンロード
+                    with open(local_path, 'wb') as f:
+                        ftp.retrbinary(f'RETR {ftp_path}', f.write)
+                        print(r'Download file is ' + local_path)
+                        break  # ダウンロード成功したらループを抜ける
+            #データがない場合は0のデータを返す
+            #それ以外のエラーは10秒間待機して再試行
+            except tuple(ftplib.all_errors) + (socket.timeout,) as e:
+                print(f"Error downloading file: {e}")
+                #dデータがない場合は0のデータを返す
+                if e.args[0].startswith('550'):
+                    print("No data available.")
+                    return np.zeros((line_number, pixel_number))
+                else:
+                    print("Wait for 10 seconds before retrying...")
+                    time.sleep(10)  # 10秒間待機して再試行
         
-    data = xr.open_dataset(ftp_base)
+    data = xr.open_dataset(ftp_base, engine='h5netcdf')
     if data is None:
         return np.zeros((line_number, pixel_number))
     os.remove(local_path)
-    
-    data_chla = data['chlor_a'].fillna(0)  # 欠損値に0を代入
-    return data_chla
+    return data
+
+def calculate_7hours_average(year, month, day, hour):
+    data_sum = None
+    count_zeros = None
+    time_input = datetime.datetime(year, month, day, hour, 0, 0)
+    time_base = time_input + datetime.timedelta(seconds=-3*60*60)
+
+    for count_i in range(7):
+        time_now = time_base + datetime.timedelta(seconds=60*60*count_i)
+        data = download_netcdf(year=time_now.year, month=time_now.month, day=time_now.day, hour=time_now.hour)
+        if data.all() == 0:
+            try:
+                data_filled = data['chlor_a'].fillna(0)  # 欠損値に0を代入
+            except KeyError as e:
+                print(f"Error: {e}")
+                print(f"Dataset keys: {data.keys()}")
+                data = np.zeros((line_number, pixel_number))
+            
+            if data_sum is None:
+                data_sum = data_filled
+                count_zeros = (data_filled == 0).astype(int)  # 0の回数をカウントするためのマスクを作成
+                #print(hour_int, r'count_zeros is initialized.', r'data exists.')
+            else:
+                data_sum += data_filled
+                count_zeros += (data_filled == 0).astype(int)
+                #print(hour_int, r'count_zeros is added.', r'data exists.')
+        else:
+            if data_sum is None:
+                data_sum = np.zeros((line_number, pixel_number))
+                count_zeros = np.ones((line_number, pixel_number)).astype(int)
+                #print(hour_int, r'count_zeros is initialized.', r'data does not exist.')
+            else:
+                count_zeros += np.ones((line_number, pixel_number)).astype(int)
+                #print(hour_int, r'count_zeros is added.', r'data does not exist.')
+        
+    divisor = 7 * np.ones((line_number, pixel_number)).astype(int) - count_zeros
+    data_average = xr.where(divisor != 0, data_sum / divisor, 0)
+
+    return data_average
+
 
 #chlaのメディアンフィルタ&vmin, vmaxの設定
 def median_filter(data, filter_size):
     data = data.astype(np.float64)
-    data = data.where(data != 0, np.nan)
+    #data = data.where(data != 0, np.nan)
     data = data.rolling(longitude=filter_size, latitude=filter_size, center=True).median()
 
     data = xr.where((data < chla_vmin) & (data != 0), chla_vmin, data)
@@ -437,15 +533,17 @@ def median_filter(data, filter_size):
 lon_plot_min, lon_plot_max, lat_plot_min, lat_plot_max = plot_width(width_lon=width_plot_1_lon, width_lat=width_plot_1_lat)
 def figure_plot(time_now, lat_array, lon_array):
     fig = plt.figure(figsize=(15, 15), dpi=100)
-    ax = fig.add_subplot(111, xlim=(lon_plot_min, lon_plot_max), ylim=(lat_plot_min, lat_plot_max), xlabel='Longitude', ylabel='Latitude')
+    ax = fig.add_subplot(111, xlabel='Longitude', ylabel='Latitude')
     ax.set_title('Start: ' + str(start_year) + '/' + str(start_month) + '/' + str(start_day) + ' ' + str(start_hour) + ':00' + ' JST, End: ' + str(end_year) + '/' + str(end_month) + '/' + str(end_day) + ' ' + str(end_hour) + ':00' + ' JST' + '\n' + 'Time: ' + str(time_now) + ' JST')
 
     #chlaのデータをダウンロード&プロット
-    data_chla = download_netcdf(year=time_now.year, month=time_now.month, day=time_now.day, hour=time_now.hour)
+    time_now_UTC_year, time_now_UTC_month, time_now_UTC_day, time_now_UTC_hour = JST_to_UTC(year=time_now.year, month=time_now.month, day=time_now.day, hour=time_now.hour)
+    data_chla = calculate_7hours_average(year=time_now_UTC_year, month=time_now_UTC_month, day=time_now_UTC_day, hour=time_now_UTC_hour)
     data_chla = median_filter(data=data_chla, filter_size=4)
     im = ax.imshow(data_chla, extent=[data_lon_min, data_lon_max, data_lat_min, data_lat_max], cmap='cool', norm=LogNorm(vmin=chla_vmin, vmax=chla_vmax))
+    ax.set_xlim(lon_plot_min, lon_plot_max)
+    ax.set_ylim(lat_plot_min, lat_plot_max)
     plt.colorbar(im, label=r'Chlorophyll-a [$\mathrm{mg / m^{3}}$]')
-    plt.subplots_adjust()
 
     #初期位置の範囲を四角で囲う
     ax.plot([start_lon_min, start_lon_min], [start_lat_min, start_lat_max], color='k', linewidth=4, alpha=0.6, label='Initial area')
@@ -457,11 +555,12 @@ def figure_plot(time_now, lat_array, lon_array):
     ax.scatter(nishinoshima_lon, nishinoshima_lat, marker='o', s=200, c='lightgrey', label='Nishinoshima', edgecolors='k', zorder=1)
 
     #結果をプロット
-    ax.scatter(lon_array, lat_array, marker='o', s=200, c='limegreen', edgecolors='k', zorder=10)
+    ax.scatter(lon_array, lat_array, marker='o', s=25, c='orange', edgecolors='k', zorder=10, alpha=0.8)
 
     ax.minorticks_on()
     ax.grid(which='both', alpha=0.3)
     ax.legend()
+    plt.subplots_adjust()
     plt.tight_layout()
     fig_name = f'{dir_figure}/figure_{time_now.year}{time_now.month:02}{time_now.day:02}{time_now.hour:02}{time_now.minute:02}.png'
     fig.savefig(fig_name)
@@ -474,14 +573,17 @@ LAT_data = LAT_data.flatten()
 LON_data = LON_data.flatten()
 figure_plot(time_now=start_time, lat_array=LAT_data, lon_array=LON_data)
 
-quit()
+
 
 #バックトラジェクトリの計算
 now_time = start_time
 while now_time > end_time:
     print(r"   ")
-    print(r"Now Calculating: " + str(now_time))
-    
+    print(r"Now Calculating: " + str(now_time) + " JST")
+
+    now_time_UTC_year, now_time_UTC_month, now_time_UTC_day, now_time_UTC_hour = JST_to_UTC(year=now_time.year, month=now_time.month, day=now_time.day, hour=now_time.hour)
+
+
     #バックトラジェクトリの計算
     LAT_data_new = np.array([])
     LON_data_new = np.array([])
@@ -489,9 +591,9 @@ while now_time > end_time:
     #並列処理
     if __name__ == '__main__':
         with Pool(parallel_number) as p:
-            results = p.starmap(runge_kutta_method, [(now_time.year, now_time.month, now_time.day, now_time.hour, lon, lat) for lon, lat in zip(LON_data, LAT_data)])
+            results = p.starmap(runge_kutta_method, [(now_time_UTC_year, now_time_UTC_month, now_time_UTC_day, now_time_UTC_hour, lon, lat) for lon, lat in zip(LON_data, LAT_data)])
             for result in results:
-                if lon_plot_min <= result[0] <= lon_plot_max and lat_plot_min <= result[1] <= lat_plot_max:
+                if result[0] == result[0] and result[1] == result[1]:
                     LAT_data_new = np.append(LAT_data_new, result[1])
                     LON_data_new = np.append(LON_data_new, result[0])
     
@@ -500,5 +602,9 @@ while now_time > end_time:
 
     #結果のプロット
     figure_plot(time_now=now_time, lat_array=LAT_data_new, lon_array=LON_data_new)
+
+    #次の時刻の初期値を設定
+    LAT_data = LAT_data_new
+    LON_data = LON_data_new
 
 print(r"Finished.")
